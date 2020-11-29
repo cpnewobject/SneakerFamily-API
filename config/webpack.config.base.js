0889abdbbde2d0f68694910326927e022ce2d5f9
chenpeng0818 <chenpeng@hupu.com>
2020-11-30 01:06:46 +0800

    feat: init

---
+++ config/webpack.config.base.js
@@ -0,0 +1,51 @@
+const path = require('path')
+const webpack = require('webpack')
+const nodeExcternals = require('webpack-node-externals')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const utils = require('./utils')
+
+const webpackconfig = {
+    target: 'node',
+    mode: 'development',
+    entry: {
+        server: path.join(utils.APP_PATH, 'index.js')
+    },
+    output:{
+        filename: '[name].bundle.js',
+        path: utils.DIST_PATH
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: {
+                    loader: 'babel-loader'
+                },
+                exclude: [path.join(__dirname, 'node_modules')]
+            }
+        ]
+    },
+    externals: [
+        nodeExcternals()
+    ],
+    plugins: [
+        new CleanWebpackPlugin(),
+        new webpack.DefinePlugin({//创建全局常量
+            'process.env': {
+                NODE_ENV: (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'prod') ? "'production'" : "'development'"
+            }
+        })
+    ],
+    node: {
+        console: true,
+        global: true,
+        process: true,
+        Buffer: true,
+        __filename: true,
+        __dirname: true,
+        setImmediate: true,
+        path: true
+    }
+}
+
+module.exports = webpackconfig
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import koa from 'koa'
+import path from 'path'
+import helmet from 'koa-helmet'
+import statics from 'koa-static'
+import koaBody from 'koa-body'
+import jsonutil from 'koa-json'
+import cors from '@koa/cors'
+import router from './routes/routes'
+import compose from 'koa-compose' //整合koa中间件
+
+const app = new koa()
+
+const middleware = compose([
+    koaBody(),
+    statics(path.join(__dirname, '../public')),
+    cors(),
+    jsonutil({ pretty: false, param: 'pretty' }),
+    helmet()
+])
+
+app.use(middleware)
+app.use(router())
+
+app.listen(3000)
